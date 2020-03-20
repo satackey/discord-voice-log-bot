29168d01e5c0df485e89b33d9262dcdba32c8826
Brian Sukhnandan <porunohoshi@gmail.com>
2020-03-20 17:26:53 -0400

    updated bot api to latest discord.py and changed requirements.txt to reflect this;

--- src/config.example.py
+++
@@ -1,5 +0,0 @@
-# The channel to send the log messages to
-CHANNEL_NAME = "voice-log"
-
-# The bot token
-BOT_TOKEN = "...token..."
--- src/main.py
+++ src/main.py
@@ -1,80 +1,95 @@
 import discord
 import asyncio
-import config
+import configparser
 
 server_channels = {} # Server channel cache
 client = discord.Client()
 
-def find_channel(server, refresh = False):
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+channel_name = config.get('Attributes', 'CHANNEL_NAME')
+BOT_TOKEN = config.get('Attributes', 'BOT_TOKEN')
+
+
+def find_channel(channel_object, refresh = False):
     """
     Find and return the channel to log the voice events to.
     
-    :param server: The server to find the channel for.
+    :param channel_object: A Channel Object to search for and return.
     :param refresh: Whether to refresh the channel cache for this server.
     """
-    if not refresh and server in server_channels:
-        return server_channels[server]
+
+    # If we aren't refreshing server cache and our specified server
+    if not refresh and channel_object in server_channels:
+        return server_channels[channel_object]
         
     for channel in client.get_all_channels():
-        if channel.server == server and channel.name == config.CHANNEL_NAME:
-            print("%s: refreshed destination log channel" % server)
-            server_channels[server] = channel
+        if channel.guild == channel_object and channel.name == channel_name:
+            print("%s: refreshed destination log channel" % channel.guild)
+            server_channels[channel_object] = channel
             return channel
             
     return None
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
 @client.event
-async def on_voice_state_update(member_before, member_after):
+async def on_ready():
+    print('\nLogged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('\n')
+
+
+@client.event
+async def on_voice_state_update(member, member_before_voice_state, member_after_voice_state):
     """
     Called when the voice state of a member on a server changes.
     
-    :param member_before: The state of the member before the change.
-    :param member_after: The state of the member after the change.
+    :param member: The member object that triggered some sort of activity.
+    :param member_before_voice_state: The voice state prior to the changes.
+    :param member_after_voice_state: The voice state after to the changes.
     """
-    server = member_after.server
+    server = member.guild
     channel = find_channel(server)
     
-    voice_channel_before = member_before.voice_channel
-    voice_channel_after = member_after.voice_channel
+    voice_channel_before = member_before_voice_state.channel
+    voice_channel_after = member_after_voice_state.channel
     
     if voice_channel_before == voice_channel_after:
         # No change
         return
-    
-    if voice_channel_before == None:
+
+    if voice_channel_before is None:
         # The member was not on a voice channel before the change
-        msg = "%s joined voice channel _%s_" % (member_after.mention, voice_channel_after.name)
+        # Discord api sets voice_channel to None if there was no channel to begin with.
+        msg = "**%s#%s** joined voice channel **_%s_**" % (member.name, member.discriminator, voice_channel_after.name)
     else:
         # The member was on a voice channel before the change
-        if voice_channel_after == None:
+        if voice_channel_after is None:
             # The member is no longer on a voice channel after the change
-            msg = "%s left voice channel _%s_" % (member_after.mention, voice_channel_before.name)
+            msg = "**%s#%s** left voice channel **_%s_**" % (member.name, member.discriminator, voice_channel_before.name)
         else:
             # The member is still on a voice channel after the change
-            msg = "%s switched from voice channel _%s_ to _%s_" % (member_after.mention, voice_channel_before.name, voice_channel_after.name)
+            msg = "**%s#%s** switched from voice channel **_%s_** to **_%s_**" % (member.name, member.discriminator,
+                                                                     voice_channel_before.name, voice_channel_after.name)
     
     # Try to log the voice event to the channel
     try:
-        await client.send_message(channel, msg)
+        await channel.send(msg)
+
     except:
         # No message could be sent to the channel; force refresh the channel cache and try again
-        channel = find_channel(server, refresh = True)
-        if channel == None:
+        channel = find_channel(server, refresh=True)
+        if channel is None:
             # The channel could not be found
-            print("Error: channel #%s does not exist on server %s." % (config.CHANNEL_NAME, server))
+            print("Error: channel #%s does not exist on server %s." % (channel_name, server))
         else:
             # Try sending a message again
             try:
-                await client.send_message(channel, msg)
+                await channel.send(msg)
             except discord.DiscordException as exception:
                 # Print the exception
-                print("Error: no message could be sent to channel #%s on server %s. Exception: %s" % (config.CHANNEL_NAME, server, exception))
+                print("Error: no message could be sent to channel #%s on server %s. Exception: %s" % (channel_name, server, exception))
 
-client.run(config.BOT_TOKEN)
+client.run(BOT_TOKEN)
